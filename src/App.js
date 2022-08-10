7c8671af829807e51664443702b7693342817ef5
ngomezap <igomez.ap@gmail.com>
2022-08-10 19:15:18 +0200

    Add basic components layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Experience } from "./components/Experience";
 import { Education } from "./components/Education";
 import { Side } from "./components/Side";
+import './styles/App.css';
 
 
 class App extends React.Component {
@@ -15,7 +16,7 @@ class App extends React.Component {
   render(){
     const {headInfo, sideInfo, education, experience} = this.props;
     return (
-      <div>
+      <div id="container">
         <Header info={headInfo}/>
         <Side info={sideInfo}/>
         <Education info={education}/>
--- src/index.js
+++ src/index.js
@@ -2,40 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const headInfo = {
-  name: 'Ignacio Gomez Aparicio',
-  position: 'Software Engineer'
-}
-
-const sideInfo = {
-  mail: 'igomez.ap@gmail.com',
-  phone: '0034 671197504',
-  linkedin: 'linkedin.com/in/ignacio-gomez-aparicio/'
-}
-
-const education = {
-  degree: 'Industrial Engineering',
-  university: 'University of Cantabria',
-  location: 'Santander',
-  endDate: 'July 2018'
-}
-
-const experience = {
-  position: 'Solar Technician',
-  company: 'Ingeteam UK ltd.',
-  initialDate: 'January 2022',
-  endDate: 'August 2022',
-  description: 'Only engineer in charge of tow solar farms of 5MW and 10MW'
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App 
-    headInfo = {headInfo}
-    sideInfo = {sideInfo}
-    education = {education}
-    experience = {experience}/>
+    <App/>
   </React.StrictMode>
 );
 
