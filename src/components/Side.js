3eba08179fcddb46fd1ee0f1d2f4dca6d7aa0dbd
ngomezap <igomez.ap@gmail.com>
2022-08-10 18:52:25 +0200

    Add working edit option to sideInfo section

--- src/components/Side.js
+++ src/components/Side.js
@@ -10,16 +10,59 @@ export class Side extends React.Component{
             linkedin: 'linkedin.com/in/ignacio-gomez-aparicio/',
             editMode: 'off'
         }
+        this.onEditButton = this.onEditButton.bind(this);
+        this.onSubmitButton = this.onSubmitButton.bind(this);
+    }
+
+    onEditButton(){
+        this.setState({
+            editMode: 'on'
+        });
+    }
+
+    onSubmitButton(e){
+        e.preventDefault();
+        const mail = e.target.parentNode.querySelector('#inputMail').value;
+        const phone = e.target.parentNode.querySelector('#inputPhone').value;
+        const linkedin = e.target.parentNode.querySelector('#inputLinkedin').value;
+
+        this.setState({
+            mail: mail,
+            phone: phone,
+            linkedin: linkedin,
+            editMode: 'off'
+        });
     }
 
     render(){
         const {mail, phone, linkedin} = this.state;
+
+        const toRender = [];
+        if(this.state.editMode === 'off'){
+            toRender.push(
+                <div>
+                    <h6>Email: {mail}</h6>
+                    <h6>Tel: {phone}</h6>
+                    <h6>LinkedIn: {linkedin}</h6>
+                    <button id='editSideBtn' onClick={this.onEditButton}>Edit</button>
+                </div>
+            )
+        }else{
+            toRender.push(
+                <div>
+                    <form>
+                        <input id='inputMail' type='text' defaultValue={mail}/>
+                        <input id='inputPhone' type='text' defaultValue={phone}/>
+                        <input id='inputLinkedin' type='text' defaultValue={linkedin}/>
+                        
+                        <button id='submitSideBtn' type='submit' onClick={this.onSubmitButton}>Submit</button>
+                    </form>
+                </div>
+            )
+        }
+        
         return(
-            <section id="sideInfo">
-                <h6>Email: {mail}</h6>
-                <h6>Tel: {phone}</h6>
-                <h6>LinkedIn: {linkedin}</h6>
-            </section>
-        )
+            <section id='sideInfo'>{toRender}</section>
+        );
     }
 }
